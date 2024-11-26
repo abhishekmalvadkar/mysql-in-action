7aaa9cf0b722f5606fa4e8fb5285e07cdb61ef65
Abhishek <60572185+abhishekmalvadkar@users.noreply.github.com>
2024-11-26 11:42:01 +0530

    Update solution.sql

--- Query of the day/Recyclable and Low Fat Products/solution.sql
+++ Query of the day/Recyclable and Low Fat Products/solution.sql
@@ -5,3 +5,8 @@ where
 	p.low_fats = 'Y' and
 	p.recyclable = 'Y'
 ;
+
+-- Learning points
+-- Think about to use char if you have fixed character count values like
+-- Some question answer like 'Y' or 'N'
+-- Gender like 'M' or 'F' or 'O'
